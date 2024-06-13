196992af49f8270516e39530a215850cda1c52b0
VNic <50366426+Leeilwoo0309@users.noreply.github.com>
2024-06-13 00:19:41 +0000

    해결해야 되는거: 30번째 줄

--- main.c
+++ main.c
@@ -1,62 +1,102 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
-void todo() {
-    printf("1: 할 일 확인");
-}
-
-char getFile() {
-    FILE *_readFile;
+#include <string.h>
+/**
+ * 파일 읽기하고 이후 작업(사용자 ㅇ입략 -> 일)
+ */
+int start() {
+    FILE *_readFile, *_booleanFile, *_writeFile, *_booleanFileWrite;
     char _fileName[15], *_buffer;
     long _fileSize;
+    char isDone[1];
 
-    printf("학번을 입력하세요: ");  
-    scanf("%s", _fileName);
-
-    _readFile = fopen(_fileName, "r");
-    if (_readFile == NULL) {
-        perror("파일 열기 실패");
-        return 1;
-    } 
+    char date[10], dataFileName[10];
+    printf("날짜를 입력해주세요 (예. 240612): ");
+    scanf("%s", date);
     
+    if (date == -1) { return 1; }
+
+    sprintf(dataFileName, "%sb", date);
+    _readFile = fopen(date, "r");
+    _booleanFile = fopen(dataFileName, "r");
+
+    if (_readFile == NULL) {
+        _writeFile = fopen(date, "w");
+        _booleanFileWrite = fopen(dataFileName, "w");
+
+        char plan[1001];
+        printf("%s의 하루 일과를 입력하세요: ", date);
+        scanf("%s", plan);
+
+        fwrite(plan, sizeof(char), strlen(plan), _writeFile);
+        fwrite("0", sizeof(char), 1, _booleanFileWrite);
+
+        fclose(_writeFile);
+        fclose(_booleanFileWrite);
+
+    } if (_booleanFile == NULL) {
+        printf("오류\n");
+        return 1;
+    }
+    
+    // 파일 ㄹㄱ기 준비
     fseek(_readFile, 0, SEEK_END);
     _fileSize = ftell(_readFile);
     rewind(_readFile);
 
-    // 파일 내용을 저장할 메모리 할당
-    _buffer = (char*)malloc(sizeof(char) * (_fileSize + 1));
-    if (_buffer == NULL) {
-        perror("메모리 할당 실패");
-        fclose(_readFile);
-        return 1;
+    // 파일 내용을 저장할 메모리 할당하는거
+    _buffer = (char*) malloc(sizeof(char) * (_fileSize + 1));
+
+    // 파일 내용 읽는거
+    fread(_buffer, sizeof(char), _fileSize, _readFile);
+    _buffer[_fileSize] = '\0';
+
+    fread(isDone, sizeof(char), 1, _booleanFile);
+    
+    printf("%s: ", _buffer);
+
+    while (1) {
+        int selection;
+
+        if (isDone[0] == '1') {
+            printf("완료됨!\n");
+        } else if (isDone[0] == '0') {
+            printf("진행중..\n");
+        }
+        
+        printf("----------------------------------------------------\n");
+        printf("0: 프로그램 종료 / 1: 계획 수정하기 / 2: 다른 날 보기 \n> ");
+
+        scanf("%d", &selection);
+
+        if (selection == 1) {
+            _booleanFileWrite = fopen(dataFileName, "w");
+            char valueToWrite = (isDone[0] == '0') ? '1' : '0';
+            fwrite(&valueToWrite, sizeof(char), 1, _booleanFileWrite);
+            fclose(_booleanFileWrite);
+
+            isDone[1] = &valueToWrite;
+        } else if (selection == 2) {
+            break;
+        } else if (selection == 0) {
+            return -1;
+        }
     }
 
-    // 파일 내용 읽기
-    fread(_buffer, sizeof(char), _fileSize, _readFile);
-    _buffer[_fileSize] = '\0'; // 문자열 끝에 null 문자 추가
-
-    // 파일 내용 출력
-    printf("파일 내용:\n%s\n", _buffer);
-
-    // 자원 해제
     fclose(_readFile);
-    return _buffer;
-    // free(_buffer);
+    free(_buffer);
 
+    return 0;
 }
 
 
 int main() {
-    printf("%s", getFile());
-
-    todo();
+    int ret;
     
-    // char* token = strtok(str, ","); // ","를 구분자로 사용하여 문자열 분할
-    // while (token != NULL) {
-    //     printf("%s\n", token);
-    //     token = strtok(NULL, ","); // 다음 분할 위치로 이동
-    // }
-
+    while (ret != -1) {
+        ret = start();
+    }
+    
     return 0;
 }
